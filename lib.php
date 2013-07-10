95194ad99cb8c0b8284288cf4fe5adcdc4e5711b
Jason Peak <jpeak5@lsu.edu>
2013-07-10 14:42:23 -0500

    corrects dependency on tcpdf directly to moodle's pdflib; in the interest of time, guts references to tcpdf constants;

--- lib.php
+++ lib.php
@@ -44,41 +44,20 @@ abstract class ExportForumPDF {
     function generate($content, $filename, $anonymize, $forum) {
         global $CFG, $USER;
 
-        require_once($CFG->dirroot . '/lib/tcpdf/tcpdf.php');
+        require_once($CFG->dirroot . '/lib/pdflib.php');
 
         $css = file_get_contents($CFG->dirroot . '/blocks/export_forum/pdf.css');
         
         make_cache_directory('tcpdf');
         
-        $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        $pdf = new pdf();
         // set document information
-        $pdf->SetCreator(PDF_CREATOR);
         $pdf->SetAuthor($USER->firstname." ".$USER->lastname);
         $pdf->SetTitle($forum->name);
         
         $h2t = new html2text($forum->intro);
         $pdf->SetSubject($h2t->get_text());
 
-        // set default header data
-//        $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 006', PDF_HEADER_STRING);
-
-        // set header and footer fonts
-        $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-        $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-
-        // set default monospaced font
-        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-
-        // set margins
-        $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-
-        // set auto page breaks
-        $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-
-        // set image scale factor
-        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
         $pdf->SetFont('Helvetica');
         
         $pdf->AddPage();
